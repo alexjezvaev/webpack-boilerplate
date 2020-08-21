2171485e2ea21e1a922b3c2be4461697d4d9f464
Aleksey <60217501+alekseizhezvaev@users.noreply.github.com>
2020-08-21 19:36:18 +0300

    first commit

---
+++ src/components/Example.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <div class="container">
+      <h1>Webpack</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: green;
+}
+</style>
